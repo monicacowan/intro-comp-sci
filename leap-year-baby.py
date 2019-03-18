1d00b59741c543bb70f4e2e1e1c9b89375fdf725
Monica_Cowan <monica@outerlabs.io>
2019-03-18 16:24:43 -0600

    Wraps up lesson 8 programs

---
+++ leap-year-baby.py
@@ -0,0 +1,61 @@
+# By Ashwath from forums
+
+# A leap year baby is a baby born on Feb 29, which occurs only on a leap year.
+
+# Define a procedure is_leap_baby that takes 3 inputs: day, month and year
+# and returns True if the date is a leap day (Feb 29 in a valid leap year)
+# and False otherwise.
+
+# A year that is a multiple of 4 is a leap year unless the year is
+# divisible by 100 but not a multiple of 400 (so, 1900 is not a leap
+# year but 2000 and 2004 are).
+
+def leapYear(year):
+    #This procedure determines whether or not a year is a leap year.
+    #Returning True means the year is a leap year; False means it is not.
+    #First, a leap year must be divisible by 4
+    if year%4 > 0:
+        return False
+        #if the year is divisible by 4 and also 100, it's a leap year
+    elif year%100 > 0:
+        return True
+        #if the year is divisible by 4 and NOT by 400, then it isn't a leap year
+    elif year%400 > 0:
+        return False
+        #and if it's divisible by 4, but not by 100 or 400, then leap year!
+    else:
+        return True
+
+def is_leap_baby(day,month,year):
+    # Write your code after this line.
+    if leapYear(year) and month == 2 and day == 29:
+        return True
+    else:
+        return False
+
+
+# The function 'output' prints one of two statements based on whether
+# the is_leap_baby function returned True or False.
+
+def output(status,name):
+    if status:
+        print "%s is one of an extremely rare species. He is a leap year baby!" % name
+    else:
+        print "There's nothing special about %s's birthday. He is not a leap year baby!" % name
+
+# Test Cases
+
+output(is_leap_baby(29, 2, 1996), 'Calvin')
+#>>>Calvin is one of an extremely rare species. He is a leap year baby!
+
+output(is_leap_baby(19, 6, 1978), 'Garfield')
+#>>>There's nothing special about Garfield's birthday. He is not a leap year baby!
+
+output(is_leap_baby(29, 2, 2000), 'Hobbes')
+#>>>Hobbes is one of an extremely rare species. He is a leap year baby!
+
+output(is_leap_baby(29, 2, 1900), 'Charlie Brown')
+#>>>There's nothing special about Charlie Brown's birthday. He is not a leap year baby!
+
+output(is_leap_baby(28, 2, 1976), 'Odie')
+#>>>There's nothing special about Odie's birthday. He is not a leap year baby!
